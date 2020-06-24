6aa8cddcaf7961c1df2fec47d7b6b46b078de54a
EvinduArunoda <earajapaksha.17@cse.mrt.ac.lk>
2020-06-24 21:26:58 +0530

    api key restored

--- functions/Email/send_email.js
+++ functions/Email/send_email.js
@@ -1,12 +1,12 @@
 const admin = require('firebase-admin');
-const DBUtil = require('../util/db_util');
+const SendGridKey = require('../util/sendGridKey');
 const Util = require('../util/util');
 const sgMail = require('@sendgrid/mail');
 
 module.exports.sendEmail =  async (data) => {
     console.log ('Starting function SEND EMAIL >>>> HERE');
 
-    sgMail.setApiKey(DBUtil.SENDGRID_API_KEY);
+    sgMail.setApiKey(SendGridKey.SENDGRID_API_KEY);
 
     return await sgMail.send(data).then(res=>res).catch(err=>err.response.body);
 
