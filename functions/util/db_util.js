e00f63f4db62654637fe1de938d0d43644f7d77f
EvinduArunoda <earajapaksha.17@cse.mrt.ac.lk>
2020-06-24 21:24:31 +0530

    api key removed

--- functions/util/db_util.js
+++ functions/util/db_util.js
@@ -4,5 +4,4 @@ exports.DRIVER = 'Drivers';
 exports.VEHICLE = 'vehicle';
 exports.TICKET = 'Ticket';
 exports.PAYMENT = 'Payment';
-exports.SENDGRID_API_KEY = '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';
 exports.CODES = 'ResetPasswordCodes';
